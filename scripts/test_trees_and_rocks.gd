f7fb46a7daa1224984c7f812d7916f97587d943b
b00kworm4lyf3 <136265146+b00kworm4lyf3@users.noreply.github.com>
2026-09-17 18:43:15 -0600

    added rock wall boundary, errors being thrown about collision shape scaling currently BUG

--- scripts/test_trees_and_rocks.gd
+++ scripts/test_trees_and_rocks.gd
@@ -3,15 +3,30 @@ extends Node3D
 @export var tree: PackedScene = preload("res://tree.tscn")
 @export var rock: PackedScene = preload("res://rock.tscn")
 @export var count := 50
-@export var area := 45.0
+@export var area := 60.0
 
 func _ready() -> void:
-    for i in count:
-        var t := tree.instantiate() as StaticBody3D
-        t.position = Vector3(randf_range(-area, area), 0.0, randf_range(-area, area))
-        add_child(t)
+	for i in count:
+		var pos = Vector3(randf_range(-area, area), 0.0, randf_range(-area, area))
+		if pos.length() < area:
+			var t := tree.instantiate() as StaticBody3D
+			t.position = pos
+			add_child(t)
 
-        if i%2 == 0:
-            var r := rock.instantiate()
-            r.position = Vector3(randf_range(-area, area), 0.0, randf_range(-area, area))
-            add_child(r)
+		if i%2 == 0:
+			pos = Vector3(randf_range(-area, area), 0.0, randf_range(-area, area))
+			if pos.length() < area:
+				var r := rock.instantiate()
+				r.position = pos
+				add_child(r)
+
+	var radVect = Vector3.FORWARD * area
+	var ang = TAU/float(count)
+	for i in range(count):
+		var r := rock.instantiate()
+		var s := Vector3(randf_range(3, 10), randf_range(3, 10), randf_range(3, 10))
+
+		r.position = radVect + Vector3(randf_range(-2,0), 0, randf_range(-2,0))
+		r.scale = s
+		radVect = radVect.rotated(Vector3.UP, ang)
+		add_child(r)
